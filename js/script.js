9b1a55fde246049256b799669dde8cafcd07d948
NoelSerraUbeda <neweonj@gmail.com>
2024-03-26 11:14:52 +0100

    loader

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const poke_container = document.getElementById('poke-container');
   const type_filter_buttons = document.getElementById('filter-buttons');
+  const loader = document.getElementById('loader'); // Nuevo
   const pokemon_count = 1025;
   const colors = {
     fire: '#ff9c54', grass: '#63bb5b', electric: '#f3d23b', water: '#4d90d5', ground: '#d97746', rock: '#bfb186', fairy: '#ec8fe6', poison: '#ab6ac8', bug: '#90c12c', dragon: '#0a6dc4', psychic: '#f97176', flying: '#8fa8dd', fighting: '#ce4069', normal: '#8f98a0', dark: '#5a5366', steel: '#aab8c2', ice: '#a3e7fd', ghost: '#705898'
@@ -10,19 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const typeImages = {
     fire: 'resources/images/types/fire.webp', grass: 'resources/images/types/grass.webp', electric: 'resources/images/types/electric.webp', water: 'resources/images/types/water.webp', ground: 'resources/images/types/ground.webp', rock: 'resources/images/types/rock.webp', fairy: 'resources/images/types/fairy.webp', poison: 'resources/images/types/poison.webp', bug: 'resources/images/types/bug.webp', dragon: 'resources/images/types/dragon.webp', psychic: 'resources/images/types/psychic.webp', flying: 'resources/images/types/flying.webp', fighting: 'resources/images/types/fighting.webp', normal: './resources/images/types/normal.webp', dark: 'resources/images/types/dark.webp', steel: 'resources/images/types/steel.webp', ice: 'resources/images/types/ice.webp', ghost: 'resources/images/types/ghost.webp'
   };
-
   const main_types = Object.keys(colors);
 
-  // Función para obtener datos de Pokémon de la API
   const fetchPokemons = async () => {
+    loader.style.display = 'block'; // Mostrar la pantalla de carga
     for (let i = 1; i <= pokemon_count; i++) {
       await getPokemon(i);
     }
-    // Mostrar los botones de filtrado después de haber cargado todos los Pokémon
     type_filter_buttons.style.display = 'block';
+    loader.style.display = 'none'; // Ocultar la pantalla de carga al completar la carga
   };
 
-  // Función para obtener datos de un Pokémon específico
   const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const res = await fetch(url);
