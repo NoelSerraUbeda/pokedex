a2f5bd3f55d18dbae2a70b11a69f9138279fc771
NoelSerraUbeda <neweonj@gmail.com>
2024-03-26 11:01:15 +0100

    Ocultar botones hasta que la api esté lista

--- js/return.js
+++ js/return.js
@@ -4,5 +4,5 @@ const returnArrow = document.getElementById('return-arrow');
 // Agregar un evento de clic a la flecha de retorno
 returnArrow.addEventListener('click', () => {
     // Redirigir a la página de la lista de Pokémon
-    window.location.href = 'pokedex.html';
+    window.location.href = 'index.html';
 });
