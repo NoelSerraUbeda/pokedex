9fbb81a14feb99f7fae5dfcfab7e70463918cd53
NoelSerraUbeda <neweonj@gmail.com>
2024-04-08 19:58:01 +0200

    arrelgo del peso y altura

--- js/pokemon_details.js
+++ js/pokemon_details.js
@@ -3,180 +3,82 @@ document.addEventListener('DOMContentLoaded', () => {
   const pokemonId = params.get('id');
   let isMegaEvolved = false;
 
-  if (pokemonId) {
-    getPokemonDetails(pokemonId);
-  }
+  if (pokemonId) getPokemonDetails(pokemonId);
 
-  // Agregar evento de clic al botón de megaevolución
   const megaEvolutionButton = document.getElementById('mega-evolution-button');
   megaEvolutionButton.addEventListener('click', () => {
-    if (pokemonId) {
-      // Alternar entre el estado base y la megaevolución
-      if (isMegaEvolved) {
-        // Si está megaevolucionado, mostrar los detalles del Pokémon base
-        getPokemonDetails(pokemonId);
-        isMegaEvolved = false; // Cambiar el estado a base
-      } else {
-        // Si está en su estado base, mostrar los detalles de la megaevolución
-        checkAndDisplayMegaEvolution(pokemonId);
-        isMegaEvolved = true; // Cambiar el estado a megaevolucionado
-      }
-    } else {
-      alert("Este Pokémon no tiene una forma megaevolucionada."); // Mostrar alerta si no hay una forma megaevolucionada
-    }
+    if (!pokemonId) return alert("Este Pokémon no tiene una forma megaevolucionada.");
+    isMegaEvolved = !isMegaEvolved;
+    isMegaEvolved ? checkAndDisplayMegaEvolution(pokemonId) : getPokemonDetails(pokemonId);
   });
 });
 
 const getPokemonDetails = async (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const pokemon = await fetchData(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  displayPokemonDetails(pokemon);
+};
+
+const fetchData = async (url) => {
   const res = await fetch(url);
-  const data = await res.json();
-  displayPokemonDetails(data);
+  return await res.json();
 };
 
-// Datos
-const getPokemonSpeciesDetails = async (id) => {
-  const url = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+const checkAndDisplayMegaEvolution = async (id) => {
+  const speciesDetails = await fetchData(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+  const megaEvolution = speciesDetails.varieties.find(variety => !variety.is_default);
+  if (!megaEvolution) return;
+  const megaPokemonId = megaEvolution.pokemon.url.split('/').slice(-2, -1)[0];
+  getPokemonDetails(megaPokemonId);
 };
 
-// Colores
-const colors = {
-  fire: '#ff9c54', grass: '#63bb5b', electric: '#f3d23b', water: '#4d90d5', ground: '#d97746', rock: '#bfb186', fairy: '#ec8fe6', poison: '#ab6ac8', bug: '#90c12c', dragon: '#0a6dc4', psychic: '#f97176', flying: '#8fa8dd', fighting: '#ce4069', normal: '#8f98a0', dark: '#5a5366', steel: '#aab8c2', ice: '#a3e7fd', ghost: '#705898'
-};
+const displayPokemonDetails = (pokemon) => {
+  const { name, sprites, types, abilities, stats, height, weight } = pokemon;
+  document.title = `Pokémon Details - ${capitalizeFirstLetter(name)}`;
 
-// Asignar color a tipo
-const getColorForType = (type) => {
-  const color = colors[type];
-  return color ? color : null;
-};
+  document.querySelector('.name').textContent = capitalizeFirstLetter(name);
+  document.getElementById('pokemon-image').src = sprites.front_default;
+  document.getElementById('pokemon-image').alt = name;
 
-// Mayusculas a los datos
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+  const typesElement = document.getElementById('pokemon-types');
+  typesElement.innerHTML = `<span>Types:</span> ${types.map(type => capitalizeFirstLetter(type.type.name)).join(', ')}`;
 
-// Display de los datos
-const displayPokemonDetails = async (pokemon) => {
-  const pokemonNameParts = pokemon.name.split('-');
-  const pokemonName = capitalizeFirstLetter(pokemonNameParts[0]);
-  document.title = `Pokémon Details - ${pokemonName}`;
+  const abilitiesElement = document.getElementById('pokemon-abilities');
+  abilitiesElement.innerHTML = `<span>Abilities:</span> ${abilities.filter(ability => !ability.is_hidden).map(ability => capitalizeFirstLetter(ability.ability.name)).join(', ')}`;
 
-  const name = document.querySelector('.name');
-  name.textContent = capitalizeFirstLetter(pokemon.name);
-
-  const image = document.getElementById('pokemon-image');
-  image.src = pokemon.sprites.front_default;
-  image.alt = pokemon.name;
-
-  const dataContainer = document.getElementById('data-container');
-
-  const types = document.getElementById('pokemon-types');
-  const pokemonTypes = pokemon.types.map(type => type.type.name.toLowerCase());
-  types.innerHTML = '<span>Types:</span> ' + pokemonTypes.map(type => capitalizeFirstLetter(type)).join(', ');
-  dataContainer.appendChild(types);
-
-  const abilities = document.getElementById('pokemon-abilities');
-  const regularAbilities = pokemon.abilities.filter(ability => !ability.is_hidden).map(ability => capitalizeFirstLetter(ability.ability.name)).join(', ');
-  abilities.innerHTML = '<span>Abilities:</span> ' + regularAbilities;
-  dataContainer.appendChild(abilities);
-
-  const hiddenAbility = pokemon.abilities.find(ability => ability.is_hidden);
+  const hiddenAbility = abilities.find(ability => ability.is_hidden);
   const hiddenAbilityElement = document.getElementById('pokemon-hidden-ability');
-
   if (hiddenAbility && !hiddenAbilityElement) {
     const hiddenAbilityParagraph = document.createElement('p');
-    hiddenAbilityParagraph.innerHTML = '<span>Hidden Ability:</span> ' + capitalizeFirstLetter(hiddenAbility.ability.name);
+    hiddenAbilityParagraph.innerHTML = `<span>Hidden Ability:</span> ${capitalizeFirstLetter(hiddenAbility.ability.name)}`;
     hiddenAbilityParagraph.id = 'pokemon-hidden-ability';
-    abilities.parentNode.insertBefore(hiddenAbilityParagraph, abilities.nextSibling);
+    abilitiesElement.parentNode.insertBefore(hiddenAbilityParagraph, abilitiesElement.nextSibling);
   } else if (!hiddenAbility && hiddenAbilityElement) {
     hiddenAbilityElement.parentNode.removeChild(hiddenAbilityElement);
   }
 
   const statsList = document.querySelector('.stats-list');
-  statsList.innerHTML = '';
-  pokemon.stats.forEach(stat => {
-    const statItem = document.createElement('li');
-    statItem.textContent = `${capitalizeFirstLetter(stat.stat.name)}: ${stat.base_stat}`;
-    statsList.appendChild(statItem);
-  });
+  statsList.innerHTML = stats.map(stat => `<li>${capitalizeFirstLetter(stat.stat.name)}: ${stat.base_stat}</li>`).join('');
 
-  const matchingType = pokemonTypes.find(type => getColorForType(type));
+  const matchingType = types.find(type => getColorForType(type.type.name));
+  if (matchingType) document.getElementById('pokemon-image').style.backgroundColor = getColorForType(matchingType.type.name);
 
-  if (matchingType) {
-    const color = getColorForType(matchingType);
-    image.style.backgroundColor = color;
-  }
+  addPokemonDescription(pokemon);
+  document.getElementById('height').textContent = `Height: ${height ? height / 10 + ' m' : "There is still no data on this aspect."}`;
+  document.getElementById('weight').textContent = `Weight: ${weight ? weight / 10 + ' kg' : "There is still no data on this aspect."}`;
+};
 
-  const speciesDetails = await getPokemonSpeciesDetails(pokemon.id);
-  const flavorTextEntries = speciesDetails.flavor_text_entries;
-  const descriptionEntry = flavorTextEntries.find(entry => entry.language.name === 'en');
-  const description = descriptionEntry ? descriptionEntry.flavor_text.replace(//g, " ") : "There is still no data on this aspect.";
+const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+
+const getColorForType = (type) => colors[type] || null;
+
+const addPokemonDescription = async (pokemon) => {
+  const speciesDetails = await fetchData(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`);
+  const flavorTextEntries = speciesDetails.flavor_text_entries.filter(entry => entry.language.name === 'en');
+  const description = flavorTextEntries.length > 0 ? flavorTextEntries[0].flavor_text.replace(//g, " ") : "There is still no data on this aspect.";
   const descriptionParagraph = document.getElementById('pokemon-description');
   descriptionParagraph.textContent = description;
-
-  const heightElement = document.getElementById('height');
-  const height = pokemon.height ? `${pokemon.height / 10} m` : "There is still no data on this aspect.";
-  heightElement.textContent = `Height: ${height}`;
-
-  const weightElement = document.getElementById('weight');
-  const weight = pokemon.weight ? `${pokemon.weight / 10} kg` : "There is still no data on this aspect.";
-  weightElement.textContent = `Weight: ${weight}`;
 };
 
-
-const checkAndDisplayMegaEvolution = async (id) => {
-  const speciesDetails = await getPokemonSpeciesDetails(id);
-
-  // Verificar si el Pokémon tiene una megaevolución
-  const hasMegaEvolution = speciesDetails.varieties.some(variety => {
-    return variety.is_default === false;
-  });
-
-  if (hasMegaEvolution) {
-    const megaEvolutionVariety = speciesDetails.varieties.find(variety => {
-      return variety.is_default === false;
-    });
-    const megaPokemonId = megaEvolutionVariety.pokemon.url.split('/').slice(-2, -1)[0];
-
-    // Verificar si el Pokémon actualmente está en su forma megaevolucionada
-    const currentUrlParams = new URLSearchParams(window.location.search);
-    const currentPokemonId = currentUrlParams.get('id');
-
-    if (currentPokemonId === megaPokemonId) {
-      // Si ya está megaevolucionado, mostrar los detalles del Pokémon normal
-      const defaultVariety = speciesDetails.varieties.find(variety => {
-        return variety.is_default === true;
-      });
-      const defaultPokemonId = defaultVariety.pokemon.url.split('/').slice(-2, -1)[0];
-      getPokemonDetails(defaultPokemonId);
-    } else {
-      // Si no está megaevolucionado, mostrar los detalles de la megaevolución
-      getPokemonDetails(megaPokemonId);
-    }
-  } else {
-    // Si el Pokémon no tiene megaevolución, no se hace nada
-  }
+const colors = {
+  fire: '#ff9c54', grass: '#63bb5b', electric: '#f3d23b', water: '#4d90d5', ground: '#d97746', rock: '#bfb186', fairy: '#ec8fe6', poison: '#ab6ac8', bug: '#90c12c', dragon: '#0a6dc4', psychic: '#f97176', flying: '#8fa8dd', fighting: '#ce4069', normal: '#8f98a0', dark: '#5a5366', steel: '#aab8c2', ice: '#a3e7fd', ghost: '#705898'
 };
-
-
-
-// Descripción
-const addPokemonDescription = (description) => {
-  const descriptionContainer = document.createElement('div');
-  descriptionContainer.classList.add('description-container');
-  const descriptionTitle = document.createElement('h3');
-  descriptionTitle.textContent = 'Description:';
-  const descriptionParagraph = document.createElement('p');
-  descriptionParagraph.textContent = description;
-  descriptionContainer.appendChild(descriptionTitle);
-  descriptionContainer.appendChild(descriptionParagraph);
-
-  const pokemonDetailsContainer = document.getElementById('pokemon-details-container');
-  pokemonDetailsContainer.appendChild(descriptionContainer);
-};
-
-
-
